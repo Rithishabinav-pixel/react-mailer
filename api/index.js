e8f2c5e5b8f1cdc75b8c549cdf356f1e739f7f6d
Rithish Abinav - Pixel <rithish.pixel@gmail.com>
2024-11-04 16:37:43 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://react-mailer-rithish.netlify.app/' // Replace with your Netlify domain
+}));
 app.use(express.json());
 
 app.post('/send-email', async (req, res) => {
